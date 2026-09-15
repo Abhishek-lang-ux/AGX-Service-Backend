16cad45fe2859c28b201e124bd927ba6848c1769
Akash Awasthi <YOUR-GITHUB-EMAIL>
2026-09-15 11:45:09 +0530

    Remove Razorpay payment routes

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
+
 import { requireAuth } from "../middleware/auth.js";
-import { createPaymentOrder, listMyPayments, verifyPayment } from "../controllers/payment.controller.js";
+
+import { listMyPayments } from "../controllers/payment.controller.js";
 
 const router = Router();
+
 router.use(requireAuth);
+
 router.get("/", listMyPayments);
-router.post("/order", createPaymentOrder);
-router.post("/verify", verifyPayment);
+
 export default router;
